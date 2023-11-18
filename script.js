7baf28400d9f0d11eb645de2d4047478e6b779c0
Pandian Sambath <85973423+pandiansambath@users.noreply.github.com>
2023-11-18 14:35:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const galleryImages = document.querySelectorAll('.gallery img');
+
+for (const image of galleryImages) {
+  image.addEventListener('click', () => {
+    // Open lightbox here
+  });
+}
